22fc351cf0d0bbfdf670e829d7c27fae41f1e22a
Viswanath-allamraju <allamrajuviswanath@gmail.com>
2021-02-24 01:19:30 +0530

    backend index.js initial setup

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
