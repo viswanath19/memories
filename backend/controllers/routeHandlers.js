551a99623dfe4ecd3f79d289d7eb1142aa04e324
Viswanath-allamraju <allamrajuviswanath@gmail.com>
2021-02-24 23:43:39 +0530

    wrapping backend changes

--- backend/controllers/routeHandlers.js
+++ backend/controllers/routeHandlers.js
@@ -1,7 +1,27 @@
-export const getPosts = (req,res) => {
-    res.send(`This is a POST Route`);
+import PostMessage from './models/postModel.js';
+
+export const getPosts = async (req,res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        console.log("These were the posts",postMessages);
+        res.status(200).json(postMessages);
+    }
+    catch (err) {
+        res.status(400).json({message:err.message});
+    }
 }
 
-export const createPost = (req,res) => {
-    res.send(`Posted Successfully`);
+export const createPost = async (req,res) => {
+
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try{
+        await newPost.save();
+        res.status(200).json(post);
+    }
+    catch(err){
+        res.status(400).json({message:err.message});
+    }
 }
