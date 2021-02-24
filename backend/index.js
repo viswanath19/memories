93a1c92c22504b688e971d409a0b6a7f1a59c1d8
Viswanath-allamraju <allamrajuviswanath@gmail.com>
2021-02-24 22:13:50 +0530

    db connection setup

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+
+const PORT = process.env.PORT || 9000;
+const mongodbUrl = "mongodb+srv://viswanath:sarma1901@cluster0.zeryh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(mongodbUrl,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(() => {app.listen(PORT,()=>{console.log(`Server started on Post ${PORT}`)})})
+.catch((e)=>{console.log("Error occured while connecting",e)});
+
+mongoose.set('useFindAndModify',false);
